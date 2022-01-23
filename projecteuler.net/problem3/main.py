07c0d34db260a48c7c2a2c2815e258ea88bdae11
franklin <2730246+devsecfranklin@users.noreply.github.com>
2022-01-23 17:04:07 -0500

    checkpoint

--- projecteuler.net/problem3/main.py
+++ projecteuler.net/problem3/main.py
@@ -6,3 +6,8 @@ What is the largest prime factor of the number 600851475143 ?
 if __name__ == "__main__":
     target = 600851475143
     
+    while n < (target + 1):
+        x = target - n
+        if n 
+
+        n = n + 1
